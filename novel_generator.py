19b127099519cd01eb399b1861bc96cfa8b3470b
SolidSea <124437100+Solid-Sea@users.noreply.github.com>
2025-03-11 13:02:25 +0800

    update

--- novel_generator.py
+++ novel_generator.py
@@ -25,7 +25,7 @@ class NovelGenerator:
             self.tf_pipeline = pipeline(
                 "text-generation",
                 model=self.ollama_cfg.get("hf_model"),
-                device_map="cuda",
+                device_map="balanced",
                 #load_in_4bit=True,
                 trust_remote_code=True,
                 #llm_int8_enable_fp32_cpu_offload=True,
